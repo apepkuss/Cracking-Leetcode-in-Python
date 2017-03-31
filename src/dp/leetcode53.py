76fc95114dfbb263e9d89d7f1e1b662dc9ecf821
apepkuss <xin.sam.liu@hotmail.com>
2017-03-30 18:13:05 -0700

    update leetcode53

--- src/dp/leetcode53.py
+++ src/dp/leetcode53.py
@@ -1,16 +1,64 @@
 
 
 class Solution(object):
-    def maxSubArray(self, nums):
+    """
+    @ Linkedin, Bloomberg, Microsoft
+
+    Array, DP, Divide and Conquer
+
+    Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+
+    For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+    the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+
+
+    Kadane's algorithm:
+
+    Initialize:
+        max_so_far = minimum integer
+        max_ending_here = 0
+
+    Loop for each element in nums:
+        max_ending_here += nums[i]
+
+        if max_so_far < max_ending_here:
+            max_so_far = max_ending_here
+
+        if max_ending_here < 0:
+            max_ending_here = 0
+
+    return max_so_far
+
+    """
+    def maxSubArray_kadane(self, nums): # O(n) time
         """
         :type nums: List[int]
         :rtype: int
         """
-        thisSum = 0
-        maxSum = -10000
+        max_so_far = -2**32-1
+        max_ending_here = 0
+
         for i in range(0, len(nums)):
-            if thisSum < 0:
-                thisSum = 0
-            thisSum = thisSum + nums[i]
-            maxSum = max(thisSum, maxSum)
-        return maxSum
+            max_ending_here = max_ending_here + nums[i]
+
+            if max_so_far < max_ending_here:
+                max_so_far = max_ending_here
+
+            if max_ending_here < 0:
+                max_ending_here = 0
+
+        return max_so_far
+
+    def maxSubArray_dp(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        curr_max = nums[0]
+        max_so_far = nums[0]
+
+        for i in range(1, len(nums)):
+            curr_max = max(nums[i], curr_max + nums[i])
+            max_so_far = max(max_so_far, curr_max)
+
+        return max_so_far
