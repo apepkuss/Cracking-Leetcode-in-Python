53a3aa4ddf133b3b37262cf92729751fb42cf6e0
apepkuss <xin.sam.liu@hotmail.com>
2017-04-25 09:27:59 -0700

    update 69

--- src/search/leetcode69_SqrtX.py
+++ src/search/leetcode69_SqrtX.py
@@ -19,9 +19,13 @@ class Solution(object):
         left, right = 1, 2**32-1
         while True:
             mid = left + (right - left) / 2
-            if mid > x / mid:
+
+            if mid * mid == x:
+                return mid
+            elif mid > x / mid:
                 right = mid - 1
             else:
-                if mid+1 > x / (mid+1):
+                # if mid < x / mid and (mid+1) > x / (mid+1), find the result
+                if mid + 1 > x / (mid + 1):
                     return mid
                 left = mid + 1
