734cf22cc46cb85a7970c6a0cd48e37b3ec7e26a
apepkuss <xin.sam.liu@hotmail.com>
2017-05-15 18:19:05 -0700

    update 103

--- src/tree/BinaryTree/leetcode103_BinaryTreeZigzagLevelOrderTraversal.py
+++ src/tree/BinaryTree/leetcode103_BinaryTreeZigzagLevelOrderTraversal.py
@@ -9,14 +9,35 @@ class TreeNode(object):
 
 
 class Solution(object):
+    """
+    @ Linkedin, Bloomberg, Microsoft
+    
+    Tree, BFS, Stack
+    
+    Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+
+    For example:
+    Given binary tree [3,9,20,null,null,15,7],
+        3
+       / \
+      9  20
+        /  \
+       15   7
+    return its zigzag level order traversal as:
+    [
+      [3],
+      [20,9],
+      [15,7]
+    ]
+    """
     def zigzagLevelOrder(self, root):
         """
         :type root: TreeNode
         :rtype: List[List[int]]
         """
         res = []
-        if root is None:
-            return res
+        if not root: return res
+
         currLevel = [root]
         reversed = False
         while len(currLevel) > 0:
@@ -28,8 +49,10 @@ class Solution(object):
                     nextLevel.append(item.left)
                 if item.right:
                     nextLevel.append(item.right)
-            if len(values) > 0:
+
+            if values:
                 res.append(values[::-1] if reversed else values)
+
             reversed = not reversed
             currLevel = nextLevel
         return res
