3145de90660846fc2d29cebe5aabe73c5e9bba24
apepkuss <xin.sam.liu@hotmail.com>
2017-12-27 16:15:21 +0800

    update leetcode 15

--- src/array/leetcode15_3Sum.py
+++ src/array/leetcode15_3Sum.py
@@ -1,4 +1,3 @@
-
 class Solution(object):
     """
     @ Amazon, Microsoft, Bloomberg, Facebook, Adobe
@@ -15,50 +14,53 @@ class Solution(object):
       [-1, -1, 2]
     ]
     """
-    def threeSum_dfs(self, nums): # DFS, but TLE
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        def dfs(depth, target, nums, valuelist):
-            if depth == 0:
-                if target == 0: res.add(tuple(valuelist))
-                return
-            if target<nums[0] or target>nums[len(nums)-1]: return
-            for i in xrange(len(nums)-depth+1):
-                dfs(depth - 1, target - nums[i], nums[i + 1:], valuelist + [nums[i]])
-        if nums == []: return []
-        nums.sort()
+
+    def threeSum_dfs(self, nums, target):
+        assert nums is not None
+        if len(nums) < 3:
+            return []
+
         res = set()
-        dfs(3, 0, nums, [])
-        return [list(t) for t in res]
+        self.dfs(nums, target, [], res)
+        return [list(elem) for elem in res]
+
+    def dfs(self, nums, target, valuelist, res):
+        if target == 0 and len(valuelist) == 3:
+            res.add(tuple(valuelist))
+        elif len(valuelist) < 3:
+            for i in range(len(nums) + len(valuelist) - 2):
+                val = nums[i]
+                self.dfs(nums[i + 1:], target - val, valuelist + [val], res)
 
     def threeSum_Generic(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        def ksum(nums,k,target):
+
+        def ksum(nums, k, target):
             # This is a generic k-sum algorithm
-            res = set() # avoid duplicates
-            i=0
-            if k==2:
-                j = len(nums)-1
-                while i<j:
-                    if nums[i]+nums[j]==target:
-                        res.add((nums[i],nums[j]))
-                        i+=1
-                    elif nums[i]+nums[j]>target:
-                        j-=1
+            res = set()  # avoid duplicates
+            i = 0
+            if k == 2:
+                j = len(nums) - 1
+                # the code in while-loop below can work because nums is sorted
+                while i < j:
+                    if nums[i] + nums[j] == target:
+                        res.add((nums[i], nums[j]))
+                        i += 1
+                    elif nums[i] + nums[j] > target:
+                        j -= 1
                     else:
-                        i+=1
-            else:   # case: k>2
-                for i in xrange(len(nums)-k+1):
-                    newtarget = target - nums[i]
-                    subresult = ksum(nums[i+1:], k-1, newtarget)
+                        i += 1
+
+            # case: k>2
+            else:
+                for i in xrange(len(nums) - k + 1):
+                    subresult = ksum(nums[i + 1:], k - 1, target - nums[i])
                     if subresult:
-                        res = res | set((nums[i],)+nr for nr in subresult)
+                        res = res | set((nums[i],) + nr for nr in subresult)
             return res
 
-        nums.sort() # O(nlog(n))
+        nums.sort()  # O(nlog(n))
         return [list(t) for t in ksum(nums, 3, 0)]
