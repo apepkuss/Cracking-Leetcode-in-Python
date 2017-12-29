4baa1d1eb4e8d5a1d815651c6f7c830a12e70a58
apepkuss <xin.sam.liu@hotmail.com>
2017-12-29 19:58:29 +0800

    update leetcode 31

--- src/array/leetcode31_NextPermutation.py
+++ src/array/leetcode31_NextPermutation.py
@@ -6,19 +6,20 @@ class Solution(object):
         :rtype: void Do not return anything, modify nums in-place instead.
         """
         i = len(nums)-1
-        # From right to left, find the first digit (PartitionNumber) which violate the increase trend
+        # From right to left, find the first element (PartitionNumber) which violates the increasing trend
         while i > 0:
             if nums[i] > nums[i-1]:
                 break
             i -= 1
+
         if i == 0:
             nums.reverse()
         else:
             # From right to left, find the first digit (ChangeNumber), which is larger than PartitionNumber;
             # then, swap ChangeNumber and PartitionNumber
-            for k in xrange(len(nums)-1, i-1, -1):
-                if nums[i-1] < nums[k]:
-                    nums[i-1], nums[k] = nums[k], nums[i-1]
+            for j in xrange(len(nums)-1, i-1, -1):
+                if nums[i-1] < nums[j]:
+                    nums[i-1], nums[j] = nums[j], nums[i-1]
                     break
 
             # Reverse all digits on the right side of partition index
