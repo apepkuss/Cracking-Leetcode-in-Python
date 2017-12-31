2ceb77c34e3c0370f2d313a9801d2d077471a195
apepkuss <xin.sam.liu@hotmail.com>
2017-12-31 22:41:31 +0800

    update leetcode 73

--- src/array/leetcode73_SetMatrixZeroes.py
+++ src/array/leetcode73_SetMatrixZeroes.py
@@ -24,7 +24,7 @@ class Solution(object):
                 if zerorows[x] or zerocols[y]:
                     matrix[x][y] = 0
 
-    def setZeroes_2(self, matrix):  # O(m*n) time, O(1) space
+    def setZeroes_2(self, matrix, verbose=False):  # O(m*n) time, O(1) space
         """
         :type matrix: List[List[int]]
         :rtype: void Do not return anything, modify matrix in-place instead.
@@ -35,22 +35,35 @@ class Solution(object):
         # the first phase, use matrix elements to set states in a top-down way. In the second phase, use states
         # to set matrix elements in a bottom-up way.
 
-        m = len(matrix)
-        n = len(matrix[0])
+        m, n = len(matrix), len(matrix[0])
 
         # store the state of column0
         col0 = False
 
         # use matrix elements to set states in a top-down way
         for i in range(m):
-            if matrix[i][0] == 0: col0 = True
+            if matrix[i][0] == 0:
+                col0 = True
+
             for j in range(1, n):
                 if matrix[i][j] == 0:
                     matrix[i][0] = matrix[0][j] = 0
 
+            if verbose:
+                print('After scan row {0}'.format(i))
+                print('row {0}: {1}'.format(i, matrix[i]))
+                print('row {0}: {1}'.format(0, matrix[0]))
+
         # use states to set matrix elements in a bottom-up way
         for i in range(m - 1, -1, -1):
+            # set the last n-1 columns from 2 to n
             for j in range(n - 1, 0, -1):
                 if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
-            if col0: matrix[i][0] = 0
+
+            # set the first column of each row
+            if col0:
+                matrix[i][0] = 0
+
+            if verbose:
+                print('row {0}: {1}'.format(i, matrix[i]))
